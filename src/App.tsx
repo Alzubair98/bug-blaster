c92993a65177a7314c082eaae6ad3676af3df281
Alzubair98 <zubairsk53@gmail.com>
2025-11-03 09:35:40 +0300

    add code to the reducer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./routes/AppRouter";
 
 export default function App() {
+  const initialState = { tickets: [] };
   return (
     <>
       <BrowserRouter>
