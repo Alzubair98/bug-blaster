4f25cf94eba261c673d0aa859728d6a743887d6d
Alzubair98 <zubairsk53@gmail.com>
2025-11-04 10:57:06 +0300

    add animation to delete

--- src/components/TicketItem.tsx
+++ src/components/TicketItem.tsx
@@ -1,4 +1,6 @@
 import type { Action, Ticket } from "../reducers/ticketReducer";
+import BlasterButton from "./BlasterButton";
+import { useRef } from "react";
 import gsap from "gsap";
 
 // send priority class outside the ticket function to avoid re-creation on each render
@@ -15,10 +17,26 @@ export default function TicketItem({
   ticket: Ticket;
   dispatch: React.Dispatch<Action>;
 }) {
-  const { id, title, description, priority } = ticket;
+  const { title, description, priority } = ticket;
+  const itemRef = useRef<HTMLDivElement>(null);
+
+  const handleDeleteAnimation = () => {
+    if (!itemRef.current) return;
+
+    gsap.to(itemRef.current, {
+      opacity: 0,
+      scale: 0.7,
+      y: 50,
+      duration: 0.5,
+      ease: "power2.inOut",
+      onComplete: () => {
+        dispatch({ type: "DELETE_TICKET", payload: ticket });
+      },
+    });
+  };
 
   return (
-    <div className="ticket-item text-white">
+    <div ref={itemRef} className="ticket-item text-white">
       <div
         className={`priority-dot ${
           priorityClass[parseInt(priority) as 1 | 2 | 3]
@@ -27,6 +45,13 @@ export default function TicketItem({
 
       <h3>{title}</h3>
       <p>{description}</p>
+
+      <BlasterButton
+        type="button"
+        className="mt-3 py-2 px-2"
+        label="Delete"
+        onClick={handleDeleteAnimation}
+      ></BlasterButton>
     </div>
   );
 }
